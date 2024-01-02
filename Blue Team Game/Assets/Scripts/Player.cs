d08e1255790d4015773a84b1ddac71ad064292f2
AnaCastillo23 <89889778+AnaCastillo23@users.noreply.github.com>
2024-01-01 18:47:16 -0800

    Testing-Playing around in Unity

---
+++ Blue Team Game/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    public float moveForce = 10f;
+
+    [SerializeField]
+    public float jumpForce = 11f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
